e0a3f20258cded5ff2646e703ba2aa2468eb9258
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-16 07:59:55 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -5,9 +5,14 @@ variable "ami_id" {
 }
 
 # optional, default value is t3.micro. user can always override
+# t3.micro, t3.small, t3.medium
 variable "instance_type" {
     type = string
     default = "t3.micro"
+    validation {
+        condition = contains(["t3.micro", "t3.medium", "t3.small"], var.instance_type)
+        error_message = "instance_type can only be t3.micro, t3.small, t3.medium"
+    }
 }
 
 # mandatory, user should supply
